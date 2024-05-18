079853a9389809e1b11ad6100ced3574144e2f11
Oleh-Liubimov <oleh.liubimov@gmail.com>
2024-05-18 21:24:17 +0300

    movie details page

--- src/pages/MovieDetailsPage/MovieDetailPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailPage.jsx
@@ -5,16 +5,18 @@ import { fetchMovieDetails } from "../../api-request";
 
 export default function MovieDetailPage() {
   const { movieId } = useParams();
-  console.log(movieId);
+  
 
-  const [movie, setMovie] = useState(null);
+  const [movie, setMovie] = useState([]);
   console.log(movie);
+  
 
   useEffect(() => {
     const fetchDetails = async () => {
       try {
         const data = await fetchMovieDetails(movieId);
-        setMovie(...data);
+        // console.log(data.data);
+        setMovie(data.data);
       } catch (error) {
         console.log(error);
       }
@@ -23,9 +25,31 @@ export default function MovieDetailPage() {
   }, [movieId]);
 
   return (
-    <div>
-      <h2>{movie.title}</h2>
-      <p>{movie.overview}</p>
+    <div className="flex items-center p-5 gap-5">
+      <div className="w-1/4">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt=""
+          className="w-full h-auto"
+        />
+      </div>
+      <div className="flex flex-col items-start gap-5 w-3/4">
+        <h2 className="text-2xl font-mono font-bold">{movie.title}</h2>
+        <p className="font-mono">{movie.tagline}</p>
+        <p className="font-mono text-xl font-bold">Overview</p>
+        <p className="font-mono">{movie.overview}</p>
+        <p className="font-mono text-xl font-bold">Average rating</p>
+        <p>{movie.vote_average}</p>
+        <p className="font-mono text-xl font-bold">Genres</p>
+        <div className="flex gap-5">
+          {movie.genres &&
+            movie.genres.map((genre) => (
+              <p key={genre.id} className="font-mono text">
+                {genre.name}
+              </p>
+            ))}
+        </div>
+      </div>
       <Outlet />
     </div>
   );
